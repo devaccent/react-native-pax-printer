a0f86f399bd8a351c8a2df65f49410a1b46965b6
Alexandru Berce <alex@devaccent.com>
2021-09-30 18:07:30 +0300

    Updated JS files with the latest changes

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,42 @@
 declare var PaxPrinter: {
-  initPrinter: () => Promise<void>,
-  setGrayLevel: (grayLevel: number) => Promise<void>,
-  addSimpleLine: (text: string) => Promise<void>,
-  printLines: () => Promise<void>,
-  cutPaper: (cutMode?: number) => Promise<void>,
+  initPrinter: () => void,
+  setGrayLevel: (grayLevel: number) => void,
+  addLine: (text: string) => void,
+  start: () => void,
+  cutPaper: (cutMode?: number) => void,
 
-  printOneLine: (text: string, cutMode?: number) => Promise<void>;
+  printLine: (text: string, cutMode?: number) => void;
+  printReceiptPage: (pageJson: string) => void;
 };
+
+type FontSize = {XXS: string, XS: string, SM: string, MD: string, LG: string, XL: string}
+type Alignment = {LEFT: string, CENTER: string, RIGHT: string}
+type TextStyle = {NORMAL: string, BOLD: string, ITALIC: string, UNDERLINE: string}
+
+export type ReceiptPageLineUnit = {
+  text: string;
+  fontSize: FontSize;
+  textStyle: TextStyle,
+  align: Alignment
+}
+
+export type ReceiptPageLine = {
+  units: ReceiptPageLineUnit[];
+}
+
+export type ReceiptPageConfig = {
+  lines: ReceiptPageLine[];
+}
 
 export declare const FULL_CUT: number;
 export declare const PARTIAL_CUT: number;
-export declare function printStrings(strings: string[], cutMode?: number): Promise<void>;
-export declare function printString(text: string, cutMode?: number): Promise<void>;
+export declare const ReceiptLineOptions: {
+  FONT_SIZES: FontSize,
+  ALIGNMENTS: Alignment,
+  TEXT_STYLES: TextStyle,
+}
+
+export declare function printString(text: string, cutMode?: number): void;
+export declare function printReceiptPage(pageConfig: ReceiptPageConfig): void;
 
 export default PaxPrinter;
--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const {PaxPrinter} = NativeModules;
 
 export const FULL_CUT = 0;
 export const PARTIAL_CUT = 1;
+const FontSizes = {XXS: 'XXS', XS: 'XS', SM: 'SM', MD: 'MD', LG: 'LG', XL: 'XL'}
+const Alignments = {LEFT: 'LEFT', CENTER: 'CENTER', RIGHT: 'RIGHT'}
+const TextStyles = {NORMAL: 'NORMAL', BOLD: 'BOLD', ITALIC: 'ITALIC', UNDERLINE: 'UNDERLINE'}
 
 const getSafeCutMode = (cutMode) => cutMode === undefined ? FULL_CUT : cutMode;
 
-export function printStrings(strings, cutMode){
-  const promise = PaxPrinter.initPrinter()
-    .then(() => PaxPrinter.setGrayLevel(3))
-
-  strings.forEach(string => {
-    promise.then(() => PaxPrinter.addSimpleLine(string))
-  });
-
-  promise.then(() => PaxPrinter.printLines())
-    .then(() => this.cutPaper(cutMode))
-
-  return promise;
+export function printString(text, cutMode) {
+  PaxPrinter.printLine(text, getSafeCutMode(cutMode));
 }
 
-export function printString(text, cutMode) {
-  return PaxPrinter.printOneLine(text, getSafeCutMode(cutMode));
+export function printReceiptPage(receiptConfig){
+  const json = JSON.stringify(receiptConfig);
+  PaxPrinter.printReceiptPage(json);
+}
+
+export const ReceiptLineOptions = {
+  FONT_SIZES: FontSizes,
+  ALIGNMENTS: Alignments,
+  TEXT_STYLES: TextStyles,
 }
 
 export default PaxPrinter;
